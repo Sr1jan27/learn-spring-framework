ee7d03406d1d31245510f0623b74725215f5513a
srijan_singh <singhsrijan809@gmail.com>
2024-06-07 07:32:28 +0530

    First Commit from eclipse

---
+++ src/main/java/com/srijan/learn_spring_framework/LearnSpringFrameworkApplication.java
@@ -0,0 +1,15 @@
+package com.srijan.learn_spring_framework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+		int j = 0;
+		
+	}
+
+}
